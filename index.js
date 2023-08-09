d1ed4188f5357cc5ca76c97664b733a712031d3e
dalilia <dalila200875yahoo.fr>
2023-08-10 00:17:21 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const s="s"
+console.log(s.startsWith('s'))
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === string.toLowerCase()
+    );
+  }
+console.log(findMatching(drivers, 'bobby'))
+
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver =>
+         driver.toLowerCase().startsWith(string.toLowerCase())
+      );
+}
+
+console.log(fuzzyMatch(drivers, 'sa'))
+
+function matchName(driver,string) {
+        return driver.filter(obj =>
+            obj.name.toLowerCase()===string.toLowerCase()
+          );
+    }
